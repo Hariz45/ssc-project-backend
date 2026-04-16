2e1c451ac59a5591a4157ca46f40021a2c3269f7
harihifihost <hariprasaths@hifihost.com>
2026-04-16 21:02:43 +0530

    fix cancel order route

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,72 +1,3 @@
-// const Order = require("../models/order");
-
-
-
-// exports.createOrder = async (req, res) => {
-//   try {
-//     const { items, totalPrice } = req.body;
-
-//     const order = await Order.create({
-//       UserId: req.user.id,
-//       items,
-//       totalPrice
-//     });
-
-//     res.json({ message: "Order placed", order });
-//     console.log("CREATE ORDER req.user:", req.user);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// };
-
-
-
-// exports.getMyOrders = async (req, res) => {
-//   try {
-//     const orders = await Order.find({ UserId: req.user.id })
-//       .populate("items.menuId");
-
-//     res.json(orders);
-//     console.log("GET MY ORDERS req.user:", req.user);
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// };
-
-
-
-// exports.getAllOrders = async (req, res) => {
-//   try {
-//     const orders = await Order.find()
-//       .populate("UserId")
-//       .populate("items.menuId");
-
-//     res.json(orders);
-
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// };
-
-
-
-// exports.updateStatus = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-
-//     const order = await Order.findByIdAndUpdate(
-//       id,
-//       { status: req.body.status },
-//       { new: true }
-//     );
-
-//     res.json({ message: "Status updated", order });
-
-//   } catch (error) {
-//     res.status(500).json(error);
-//   }
-// };
-
 const Order = require("../models/order");
 
 exports.createOrder = async (req, res) => {
@@ -131,6 +62,27 @@ exports.updateStatus = async (req, res) => {
     res.json({ message: "Status updated", order });
   } catch (error) {
     console.log("UPDATE STATUS ERROR:", error);
+    res.status(500).json(error);
+  }
+};
+
+exports.cancelMyOrder = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const order = await Order.findOneAndUpdate(
+      { _id: id, userId: req.user.id },
+      { status: "Cancelled" },
+      { new: true }
+    );
+
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    res.json({ message: "Order cancelled", order });
+  } catch (error) {
+    console.log("CANCEL ORDER ERROR:", error);
     res.status(500).json(error);
   }
 };
